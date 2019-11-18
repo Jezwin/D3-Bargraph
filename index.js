ecd665017bd66b8fc67a50abe0717c9f148ba6ab
Jezwin <jezwn@outlook.com>
2019-11-18 20:01:07 +0530

    Introduced switching between horizontal and vertical bargraphs

--- index.js
+++ index.js
@@ -16,56 +16,99 @@ const width = +svg.attr('width');
 const render = data => {
   
   const title = 'Top 10 populous countries';
-  
-  //Value accessors. Limits the specifity of code to xValue and yValue
-  const xValue = d => d.population;
   const xAxisLabel = 'Population';
-  const yValue = d => d.country;
   const yAxisLabel = 'Country';
+  const direction = 'horizontal';
+  
+
   
   const margin = {top: 60, right: 40, bottom: 70, left: 180};
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
   
-  //Maps values from domain to corresponding values in range(Data Space to Screen Space)
-  const xScale = scaleLinear()
+  //Value accessors. Limits the specifity of code to xValue and yValue
+  const xValue = direction == 'horizontal'? d => d.population : d => d.country;
+ 	const yValue = direction == 'horizontal'? d => d.country: d => d.population;
+ 	
+  //console.log(xValue)
+ 
+  const setScales = () => {
+			if(direction == 'horizontal') {
+				return setHorizontalScales();
+			} else {
+				return setVerticalScales();
+			}
+		}
+	
+  const setHorizontalScales = () => {
+    
+  	 //Maps values from domain to corresponding values in range(Data Space to Screen Space)
+  const xScaleEval = scaleLinear()
   	.domain([0, max(data, xValue)])
   	.range([0, innerWidth])
   	.nice(); //Smooth edging of axes
   
   //Separate the bars and determine their heights
-  const yScale = scaleBand()
+  const yScaleEval = scaleBand()
   	.domain(data.map(yValue))
   	.range([0, innerHeight])
   	.padding(0.1);
   
-  //The axes space
+    return {yScale: yScaleEval, xScale: xScaleEval};
+    
+  }
+  
+   const setVerticalScales = () => {
+    
+  	 //Maps values from domain to corresponding values in range(Data Space to Screen Space)
+  const yScaleEval = scaleLinear()
+  	.domain([max(data, yValue),0])
+  	.range([0, innerHeight])
+  	.nice(); //Smooth edging of axes
+  
+  //Separate the bars and determine their heights
+  const xScaleEval = scaleBand()
+  	.domain(data.map(xValue))
+  	.range([0, innerWidth])
+  	.padding(0.1);
+  
+    return {yScale: yScaleEval, xScale: xScaleEval};
+    
+  }
+  
+  
+  
+   const xScale = setScales().xScale;
+   const yScale = setScales().yScale;
+    
+    //The axes space
   const g = svg.append('g')
   	.attr('transform', 'translate('+margin.left+', '+margin.top+')');
   
   //Formatting numbers
-  const xAxisTickFormat = number =>
+  const theAxisTickFormat = number =>
   	format('.3s')(number)
   		.replace('G', 'B');
  
   const xAxis =  axisBottom(xScale)
-  	.tickFormat(xAxisTickFormat)
   	//The vertical grid
   	.tickSize(-innerHeight)
   	.tickPadding(10); //Label distance from X axis
-  
-  
+ 
   const yAxis = axisLeft(yScale)
   	//The horizontal grid
   	.tickSize(-innerWidth)
   	.tickPadding(10); //Label distance from Y axis;
   
+   direction == 'horizontal'? xAxis.tickFormat(theAxisTickFormat) : yAxis.tickFormat(theAxisTickFormat);
+  
   
   //Call to Y axis
   const yAxisG = g.append('g').call(yAxis);
-  //Remove '.tick line' to show the horizontal grid
-  yAxisG.selectAll('.domain, .tick line').remove();
+  //Remove/Add '.tick line' to show/hide the horizontal grid
+  yAxisG.selectAll('.domain').remove();
   
+  //Y Axis Label
 	yAxisG.append('text')
   	.attr('y', -110)
   	.attr('class', 'axis-label')
@@ -82,6 +125,7 @@ const render = data => {
   // Remove lines and ticks(not ticks in this)
   xAxisG.select('.domain').remove();
   
+  //X Axis Label
   xAxisG.append('text')
   	.attr('y', 60)
   	.attr('class', 'axis-label')
@@ -89,19 +133,45 @@ const render = data => {
   	.attr('fill', 'black')
   	.text(xAxisLabel);
   
+  
+   const createBar = () => {
+			if(direction == 'horizontal') {
+				createHorizontalBar();
+			} else {
+				createVerticalBar();
+			}
+		}
+ 	
+  
+  const createHorizontalBar = () => {
+    
 	g.selectAll('rect').data(data)
   	.enter().append('rect')
   	.attr('y', d => yScale(yValue(d)))
   	.attr('width', d => xScale(xValue(d)))
-  	.attr('height', yScale.bandwidth())
+  	.attr('height', yScale.bandwidth());
+  }
   
-  const xTitle = innerWidth/2;
+   const createVerticalBar = () => {
+    
+	g.selectAll('rect').data(data)
+  	.enter().append('rect')
+  	.attr('x', d=> xScale(xValue(d)))
+  	.attr('y', d => yScale(yValue(d)))
+  	.attr('height', d => innerHeight-yScale(yValue(d)))
+  	.attr('width', xScale.bandwidth())
+  }
+   
+  
+  
+  createBar();
   g.append('text')
   	.attr('class', 'title')
   	.attr('y', -10)
   	.text(title);
 };
 
+//Fetch the csv and calls the render() method
 csv('data.csv').then(data => { 
   //Returns a promise that resolves when data is loaded
   data.forEach(d => {
